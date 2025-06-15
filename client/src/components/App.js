16e3335e3c386eb2af49e327fab2fb20655a80a0
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2025-06-14 22:03:02 -0700

    started to populate database, needs more data

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Squanch Store</h1>;
+  return <h1 className="main-title">JMB Market</h1>;
 }
 
 export default App;
